280336cf952a619fd34f94c88c591c8e65498703
Evgenchuwu <evgenikurtev@abv.bg>
2024-10-03 12:58:25 +0200

    CTF added packages
    dont add burp(optionally)

--- packages/CTF.nix
+++ packages/CTF.nix
@@ -1,4 +1,6 @@
-# TODO: CTF packages, sandbox, sec shell configuration 
+# TODO: CTF packages
+# TODO: Sandbox
+# TODO: cyber sys conf
 { lib, config,pkgs, ... }: {                                  
   options = {                                            
     CTF.enable = lib.mkEnableOption "enbable CTF packages"; 
@@ -6,6 +8,11 @@
   config = lib.mkIf config.CTF.enable { 
     home.packages = with pkgs; [
       nmap
+      hashcat
+      john
+      aircrack-ng
+      audacity
+      hydra
     ];
   };
 }
