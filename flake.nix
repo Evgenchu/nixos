1144d77ad535f525413a4b184cdeed3ea6464b85
Evgenchuwu <evgenikurtev@abv.bg>
2024-10-17 10:24:19 +0200

    stable packages imported to home-mannager(stable module fixed)

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.evgeni = import ./home.nix;
+              home-manager.extraSpecialArgs = specialArgs;
             }
           ];
         };
