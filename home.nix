7cfbf2a857036e66c22a60971a05b70bbd358c89
Evgenchuwu <evgenikurtev@abv.bg>
2024-11-21 11:50:05 +0100

    colorscheme + first steps

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./packages/packagebundle.nix
+    ./nixvim/default.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should                                                             
   # manage.                                                                                                                               
