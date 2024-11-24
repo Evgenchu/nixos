4864bb226144870512728c7ea49b50613505e3c1
Evgenchuwu <evgenikurtev@abv.bg>
2024-11-24 20:08:25 +0100

    added nvim normal again

--- packages/packagebundle.nix
+++ packages/packagebundle.nix
@@ -3,9 +3,11 @@
   imports = [
     ./zsh.nix
     ./CTF.nix
+    ./nvim.nix
     ./stable.nix
   ];
-
+  
+  nvim.enableModule = lib.mkDefault true;
   zsh.enableModule = lib.mkDefault true;
   CTF.enable = lib.mkDefault true;
   stable.enable = lib.mkDefault true;
