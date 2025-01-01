e814237eb97c7c496899061cbc3e10b16b9f91e8
Evgenchuwu <evgenikurtev@abv.bg>
2025-01-01 16:54:11 +0100

    removed CTF packages

--- packages/packagebundle.nix
+++ packages/packagebundle.nix
@@ -1,12 +1,11 @@
 # NOTE: try to do something "juicy" here cmon
-{ lib, ... }: {
+{ lib, ... }:
+{
   imports = [
     ./zsh.nix
-    ./CTF.nix
     ./stable.nix
   ];
-  
+
   zsh.enableModule = lib.mkDefault true;
-  CTF.enable = lib.mkDefault true;
   stable.enable = lib.mkDefault true;
 }
